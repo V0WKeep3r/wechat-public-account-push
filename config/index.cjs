2048d16bd6f5867b5d1f9c2e8ff440bf5049af8c
V0WKeep3r <78677532+V0WKeep3r@users.noreply.github.com>
2022-11-23 14:49:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -53,7 +53,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oLS0b6w992EWmZ8CG-KhRJleDLBo',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'IqPlf1cDc61wnChHjFZqVLYpuKHGszwhZfJpzK3BHjo',
+      useTemplateId: '4jmqN_qafI5MNoQFLbwe6gYw6oYqc6pDI41oPtEll20',
       // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '12-19',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
